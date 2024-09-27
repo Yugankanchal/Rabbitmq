521122d4794727645ece5c4f4fe479ab6cddd760
Yugank Anchal <yugankanchal27@gmail.com>
2024-09-27 14:48:33 +0530

    making a basic producer and consumer

---
+++ consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "LetterBox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, et) =>
+{
+    var body = et.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Received: {message}");
+};
+
+channel.BasicConsume(
+    queue: "LetterBox",
+    autoAck: true,
+    consumer: consumer
+);
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
---
+++ producer/Program.cs
@@ -0,0 +1,28 @@
+﻿    using System;
+    using System.Text;
+    using RabbitMQ.Client;
+
+
+    var factory = new ConnectionFactory()
+    {
+        HostName = "localhost"
+    };
+
+    var connection = factory.CreateConnection();
+    var channel = connection.CreateModel();
+
+    channel.QueueDeclare(
+        queue: "LetterBox",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
+
+    var message = "This is my First message Hello Everyone";
+
+    var encodeMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "LetterBox", null, encodeMessage);
+
+    Console.WriteLine($"published message {message}");
